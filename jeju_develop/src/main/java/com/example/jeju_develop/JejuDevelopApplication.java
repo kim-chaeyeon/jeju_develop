3a776884ad1462ef906a854dc3caf1772a0455f1
kim-chaeyeon <20212621@gm.hannam.ac.kr>
2024-10-22 17:51:50 +0900

    첫세팅

---
+++ jeju_develop/src/main/java/com/example/jeju_develop/JejuDevelopApplication.java
@@ -0,0 +1,14 @@
+package com.example.jeju_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuDevelopApplication.class, args);
+	}
+
+}
+
---
+++ jeju_develop/src/test/java/com/example/jeju_develop/JejuDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeju_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
